ef4e9efb6d4c384237011e1784383c01a920717a
CY <31556438+c3p0cy@users.noreply.github.com>
2017-10-04 20:12:52 +0800

    TwoSum: onePhaseHashMap, twoPhaseHashMap

--- src/main/java/tw/cy/study/leetcode/TwoSum.java
+++ src/main/java/tw/cy/study/leetcode/TwoSum.java
@@ -1,5 +1,11 @@
 package tw.cy.study.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * https://leetcode.com/problems/two-sum/description/
  * 
@@ -8,14 +14,72 @@ package tw.cy.study.leetcode;
  */
 public class TwoSum {
 
-	public int[] twoSum(int[] nums, int target) {
-        for(int i = 0, len = nums.length - 1; i < len; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        throw new IllegalArgumentException("No twoSum solution");
-    }
+	public static void main(String[] args) {
+		TwoSum ts = new TwoSum();
+		int[] nums = new int[] { 2, 7, 11, 15 };
+		int target = 9;
+
+		int[] result = ts.twoPhaseHashMap(nums, target);
+		System.out.println(Arrays.toString(result));
+	}
+
+	/**
+	 * Time complexity: O(n)
+	 * Space complexity: O(n)
+	 * 
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] onePhaseHashMap(int[] nums, int target) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0, len = nums.length; i < len; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				return new int[] { map.get(complement), i };
+			} else {
+				map.put(nums[i], i);
+			}
+		}
+		throw new IllegalArgumentException("No twoSum solution");
+	}
+
+	/**
+	 * Time complexity: O(n)
+	 * Space complexity: O(n)
+	 * 
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] twoPhaseHashMap(int[] nums, int target) {
+		HashMap<Integer, Integer> map = (HashMap<Integer, Integer>) IntStream.range(0, nums.length).boxed()
+				.collect(Collectors.toMap(i -> nums[i], Function.identity(), (p1, p2) -> p2));
+		for (int i = 0, len = nums.length; i < len; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement) && (map.get(complement) != i)) {
+				return new int[] { i, map.get(complement) };
+			}
+		}
+		throw new IllegalArgumentException("No twoSum solution");
+	}
+
+	/**
+	 * Time complexity: O(n^2)
+	 * Space complexity: O(1)
+	 * 
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] bruteForce(int[] nums, int target) {
+		for (int i = 0, len = nums.length - 1; i < len; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		throw new IllegalArgumentException("No twoSum solution");
+	}
 }
--- src/test/java/tw/cy/study/leetcode/TwoSumTest.java
+++ src/test/java/tw/cy/study/leetcode/TwoSumTest.java
@@ -7,21 +7,71 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class TwoSumTest {
+	
+	@Test
+	public void testOnePhaseHashMap() {
+		TwoSum twoSum = new TwoSum();
+		int[] nums = new int[] { 2, 7, 11, 15 };
+		int target = 9;
+		int[] expected = new int[] { 0, 1 };
+		int[] result = twoSum.onePhaseHashMap(nums, target);
+		assertTrue(Arrays.toString(result), Arrays.equals(result, expected));
+		
+		nums = new int[] { 0, 4, 3, 0 };
+		target = 0;
+		expected = new int[] { 0, 3 };
+		result = twoSum.onePhaseHashMap(nums, target);
+		assertTrue(Arrays.toString(result), Arrays.equals(result, expected));
+		
+		nums = new int[] { 1, 4, 4, 3 };
+		target = 8;
+		expected = new int[] { 1, 2 };
+		result = twoSum.onePhaseHashMap(nums, target);
+		assertTrue(Arrays.toString(result), Arrays.equals(result, expected));
+	}
 
 	@Test
-	public void test() {
+	public void testTwoPhaseHashMap() {
 		TwoSum twoSum = new TwoSum();
-		int[] nums = new int[] {2, 7, 11, 15};
+		int[] nums = new int[] { 2, 7, 11, 15 };
 		int target = 9;
-		int[] expected = new int[] {0, 1};
-		int[] result = twoSum.twoSum(nums, target);
-		assertTrue(Arrays.equals(result, expected));
-		
-		nums = new int[] {0,4,3,0};
+		int[] expected = new int[] { 0, 1 };
+		int[] result = twoSum.twoPhaseHashMap(nums, target);
+		assertTrue(Arrays.toString(result), Arrays.equals(result, expected));
+
+		nums = new int[] { 0, 4, 3, 0 };
 		target = 0;
-		expected = new int[] {0, 3};
-		result = twoSum.twoSum(nums, target);
-		assertTrue(Arrays.equals(result, expected));
+		expected = new int[] { 0, 3 };
+		result = twoSum.twoPhaseHashMap(nums, target);
+		assertTrue(Arrays.toString(result), Arrays.equals(result, expected));
+		
+		nums = new int[] { 1, 4, 4, 3 };
+		target = 8;
+		expected = new int[] { 1, 2 };
+		result = twoSum.twoPhaseHashMap(nums, target);
+		assertTrue(Arrays.toString(result), Arrays.equals(result, expected));
+	}
+	
+	@Test
+	public void testBruteForce() {
+		TwoSum twoSum = new TwoSum();
+		int[] nums = new int[] { 2, 7, 11, 15 };
+		int target = 9;
+		int[] expected = new int[] { 0, 1 };
+		int[] result = twoSum.bruteForce(nums, target);
+		assertTrue(Arrays.toString(result), Arrays.equals(result, expected));
+
+		nums = new int[] { 0, 4, 3, 0 };
+		target = 0;
+		expected = new int[] { 0, 3 };
+		result = twoSum.bruteForce(nums, target);
+		assertTrue(Arrays.toString(result), Arrays.equals(result, expected));
+		
+		nums = new int[] { 1, 4, 4, 3 };
+		target = 8;
+		expected = new int[] { 1, 2 };
+		result = twoSum.bruteForce(nums, target);
+		assertTrue(Arrays.toString(result), Arrays.equals(result, expected));
 	}
 
 }
